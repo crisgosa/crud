c9fba4423fa98fcf161533c84b199d19de7e6319
crisgosa <c.gonzalez7337@gmail.com>
2021-02-25 00:35:53 -0500

    finish memory crud

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 
 import React, {useState} from 'react'
-import { isEmpty } from 'lodash'
-import shortid from 'shortid'
+import { isEmpty, size } from 'lodash'
+import shortid, { isValid } from 'shortid'
 
 
 function App() {
   const [task, setTask] = useState("")
   const [tasks, setTasks] = useState([])
+  const [editMode, setEditMode] = useState(false)
+  const [id, setId] = useState("")
+  const [error, setError] = useState(null)
+
+  const validForm = () =>{
+    let isValid = true
+    setError(null)
+    if(isEmpty(task)){
+      setError("You need add a task")
+      isValid = false
+    }    
+    return isValid
+  }
 
   /*Valida que el text tenga valores, no esté vacío*/
   const addTask = (e) => {
     e.preventDefault()
-    if(isEmpty(task)){
-      console.log("Task Empty")
+    if(!validForm()){
       return
     }
-    
     const newTask = {
       id: shortid.generate(),
       name: task
@@ -24,30 +35,71 @@ function App() {
     setTask("")
   }
 
+  /*Método para guardar una tarea editada */
+  const saveTask = (e) => {
+    e.preventDefault()
+    if(!validForm()){
+      return
+    }
+    const editedTasks = tasks.map(item => item.id === id ? {id, name: task} : item)
+    setTasks(editedTasks)
+    setEditMode(false)
+    setTask("")
+    setId("")
+  }
+
+  /*Método para eliminar tareas, guarda todas las tareas que no tengan el id que se desea eliminar y llena nuevamente el vector de tareas*/
+  const deleteTask = (id) => {
+    const filteredTasks = tasks.filter(task => task.id !== id)
+    setTasks(filteredTasks)
+  }
+
+  /*Activar el formulario editar */
+  const editTask = (theTask) => {
+    setTask(theTask.name)
+    setEditMode(true)
+    setId(theTask.id)
+  }
+
   return (
     <div className= "container mt-5">
       <h1>Tasks</h1>
       <hr/>
       <div className="row">
         <div className="col-8">
-          <h4 className="text-center">Task List</h4>
-          <ul className="list-group">
-          {
-            tasks.map((task) => (
-              <li className="list-group-item" key={task.id}>
-                  <span className="lead">{task.name}</span>
-                  <button className="btn btn-danger btn-sm float-right mx-2">Delete</button>
-                  <button className="btn btn-warning btn-sm float-right">Edit</button>
-              </li>
-            ))    
+          <h4 className="text-center">Tasks List</h4>
+            {              
+              size(tasks) == 0 ?(
+                <li className="list-group-item">You haven't pending tasks</li>
+              ) : (
+                <ul className="list-group">
+                  {
+                    tasks.map((task) => (
+                      <li className="list-group-item" key={task.id}>
+                          <span className="lead">{task.name}</span>
+                          <button 
+                          className="btn btn-danger btn-sm float-right mx-2"
+                          onClick = {() => deleteTask(task.id)}
+                          >Delete                  
+                          </button>
 
-          }
-
-          </ul>          
+                          <button 
+                          className="btn btn-warning btn-sm float-right"
+                          onClick = {() => editTask(task)}
+                          >Edit
+                          
+                          </button>
+                      </li>
+                    ))
+                  }
+                </ul>                
+              )
+            }
         </div>
         <div className="col-4">
-          <h4 className="text-center">Form</h4>
-          <form onSubmit={addTask}>
+          <h4 className="text-center">{editMode ? "Edit Task" : "Add Task"}</h4>
+          <form onSubmit={editMode ? saveTask : addTask}>
+            {error && <span className="text-danger">{error}</span>}
             <input
               type="text"
               className="form-control mb2"
@@ -55,8 +107,8 @@ function App() {
               onChange={(text)=>setTask(text.target.value)} /*Lleva al setTask el valor del text*/
               value={task}/*Lee el valor en task y lo pone en el value del botón*/
             />
-            <button className="btn btn-dark btn-block" type="submit">
-              Add
+            <button className= {editMode ? "btn btn-warning btn-block" : "btn btn-dark btn-block"} type="submit">
+            {editMode? "Save" : "Add"}
             </button>
           </form>
         </div>
